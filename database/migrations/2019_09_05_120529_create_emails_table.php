8cd8902c81a47d01975a1f1e27473300116a50b4
David <h_david@live.de>
2019-09-05 14:15:56 +0200

    Add table emails
    Add emails table to database
    missing content field (I dont know how to create this type of object)

---
+++ database/migrations/2019_09_05_120529_create_emails_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subject');
+            $table->string('sendto');
+            $table->string('sendfrom');
+            $table->text('copysendto');
+            $table->text('msg');
+            $table->boolean('is_important');
+            $table->boolean('is_spam');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
